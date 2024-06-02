cd4c7f5cd8f49b3ff439a6ff6a5295ce219bf876
shaisabm <shaisabmistry@gmail.com>
2024-06-01 20:07:25 -0700

    Automate run the sync function

---
+++ studentBase/management/command/task_runner.py
@@ -0,0 +1,8 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from .tasks import sync_from_sheet_to_profile
+
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(sync_from_sheet_to_profile,'interval',seconds = 20)
+    scheduler.start()
