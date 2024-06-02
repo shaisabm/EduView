7206396749b5a394a56e74b24ea6073cb11706cb
shaisabm <shaisabmistry@gmail.com>
2024-06-01 19:30:53 -0700

    Added student profile and update profile page

--- studentBase/urls.py
+++ studentBase/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('students/',views.student,name = '`student`')
+    path('student/<int:id>/',views.profile,name = 'profile'),
+    path('update-profile/<int:id>',views.update_profile, name='update-profile')
+
 ]
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
