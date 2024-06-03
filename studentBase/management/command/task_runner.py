628eb22124cf9e615b61830d0882019c8aca08aa
shaisabm <shaisabmistry@gmail.com>
2024-06-02 17:50:31 -0700

    Changed sync interval from 20s to 30s

--- studentBase/management/command/task_runner.py
+++ studentBase/management/command/task_runner.py
@@ -4,5 +4,5 @@ from .tasks import sync_from_sheet_to_profile
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(sync_from_sheet_to_profile,'interval',seconds = 20)
+    scheduler.add_job(sync_from_sheet_to_profile,'interval',seconds = 30)
     scheduler.start()
