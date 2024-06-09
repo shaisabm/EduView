98c35c07b25048b2a47a325157c439a0b6cb174c
Shaisab Mistry <shaisabmistry@gmail.com>
2024-06-09 11:53:35 -0400

    Convert pdf to png

--- studentBase/api/views.py
+++ studentBase/api/views.py
@@ -1,10 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse
 from pdf2image import convert_from_path
-from django.core.files.base import ContentFile
-from PIL import Image
-import io
+import os
+from django.http import HttpResponse
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -16,12 +14,23 @@ def getRoutes(request):
 
 @api_view(['POST'])
 def getPdfToPng(request):
-    pdf_file = request.FILES['pdf_file']
-    images = convert_from_path(pdf_file.temporary_file_path())
-    if images:
-        image_bytes = io.BytesIO()
-        images[0].save(image_bytes, format="PNG")
-        image_bytes.seek(0)
-        return HttpResponse(image_bytes, content_type='image/png')
-    else:
-        return Response({"error": "No images found in PDF"}, status=400)
+    try:
+        pdf_file = request.FILES['pdf_file']
+        temp_path = 'static/temp/'
+        with open(os.path.join(temp_path,str(pdf_file)),'wb+') as temp:
+            for chunk in pdf_file.chunks():
+                temp.write(chunk)
+
+        images= convert_from_path(os.path.join(temp_path,str(pdf_file)),poppler_path='/opt/homebrew/Cellar/poppler/24.04.0/bin')
+        pdf_file = str(pdf_file)
+        image_name = pdf_file[:len(list(pdf_file))-4]
+        for i in range(len(images)):
+            images[i].save(os.path.join(temp_path,str(image_name)+'.jpg'),'JPEG')
+
+        image_path = os.path.join(temp_path,image_name+'.jpg')
+        with open(image_path,'rb') as img:
+            return HttpResponse(img.read(),content_type='image/jpeg')
+    except Exception as e:
+        print(e)
+        pass
+
