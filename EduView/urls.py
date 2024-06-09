f79bfe7ff215cf21b4c9f2f66dc8683c5dd455b2
Shaisab Mistry <shaisabmistry@gmail.com>
2024-06-09 11:53:20 -0400

    error 404 after debug = False fixed

--- EduView/urls.py
+++ EduView/urls.py
@@ -2,9 +2,17 @@
 from django.contrib import admin
 from django.urls import path,include
 
+from django.urls import re_path
+from django.conf import settings
+from django.views.static import serve
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('studentBase.urls')),
-    path('api/',include('studentBase.api.urls'))
+    path('api/',include('studentBase.api.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
