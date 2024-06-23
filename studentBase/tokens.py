1ffa7a484ab373365850507e8f37d14dee02b65a
Shaisab Mistry <shaisabmistry@gmail.com>
2024-06-22 20:55:10 -0400

    Generate the email verification token

--- studentBase/tokens.py
+++ studentBase/tokens.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
-class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+class GenerateToken(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return str(user.pk) + str(timestamp)
 
-account_activation_token = AccountActivationTokenGenerator()
+
+
+
+token_generator = GenerateToken()
