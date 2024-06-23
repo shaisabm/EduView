c413bd6ea01e6df313ff435f0f199c1acbf873fc
Shaisab Mistry <shaisabmistry@gmail.com>
2024-06-22 21:02:22 -0400

    add User to the admin panel/

--- studentBase/admin.py
+++ studentBase/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, TeacherProfile
+from .models import Profile, User
 
 admin.site.register(Profile)
-admin.site.register(TeacherProfile)
+admin.site.register(User)
