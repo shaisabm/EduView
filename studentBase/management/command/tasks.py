ccc2d77639fc0f9f5a3b37e40ab2c78903be9ecd
Shaisab Mistry <shaisabmistry@gmail.com>
2024-06-24 21:45:10 -0400

    Do not crash the site when maximum per minute api request reached

--- studentBase/management/command/tasks.py
+++ studentBase/management/command/tasks.py
@@ -4,21 +4,24 @@ from studentBase.views import SHEET_NAME
 
 
 def sync_from_sheet_to_profile():
-    students,_ = get_all_students(SHEET_NAME)
-    student_ids = get_all_ids(SHEET_NAME)
+    try:
+        students,_ = get_all_students(SHEET_NAME)
+        student_ids = get_all_ids(SHEET_NAME)
+        for student in students:
+            student_id = student['Student_ID']
+            if type(student_id) == int:
+                Profile.objects.update_or_create(
+                    Student_ID=student['Student_ID'],
+                    defaults=student
+                )
 
-    for student in students:
-        student_id = student['Student_ID']
-        if type(student_id) == int:
-            Profile.objects.update_or_create(
-                Student_ID=student['Student_ID'],
-                defaults=student
-            )
 
+        for object in Profile.objects.all():
+            if object.Student_ID not in student_ids:
+                object.delete()
 
-    for object in Profile.objects.all():
-        if object.Student_ID not in student_ids:
-            object.delete()
+    except: print('Maximum api request exceed')
+
 
 
 
